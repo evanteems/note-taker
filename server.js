ada26d865116ab2f930c3c75f7073d6891084437
Evan Teems <evanteems40@yahoo.com>
2021-02-28 12:51:45 -0800

    done

--- server.js
+++ server.js
@@ -5,47 +5,52 @@ const express = require('express');
 
 const path = require('path');
 
-const jay = require('jay');
+//const jay = require('jay');
 
-const generateUId = require('generate-unique-id');
+const genereateUniqueId = require('generate-unique-id');
 
-const notes = require('./db/db.json');
+const jsonNotes = require('./db/db.json');
 const {json} = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(jay("dev"));
+//app.use(jay("dev"));
 
 app.get('/', (req, res) => {
     res.sendFiles(path.join(__dirname, './public/index.html'));
 });
 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    res.json(jsonNotes);
 });
 
 app.post('/api/notes', function (req, res) {
     let note = {
         title: req.body.title,
         text: req.body.text,
-        id: generateUId({
+        id: genereateUniqueId({
             length: 9,
             useLetters: false
         })
     };
 
     console.log(note);
-    notes.push(note);
+    jsonNotes.push(note);
 
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (error) => {
+    fs.writeFile('./db/db.json', JSON.stringify(jsonNotes), (error) => {
         if (error) {
             console.log(error);
         }
-        res.send(notes);
+        res.send(jsonNotes);
     });
 });
 
